6be6d01d7e066374ebab77c43f0e75005494f84d
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-05 17:27:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import InputSearch from './components/InputSearch'
-import FilterResult from './components/FilterResult'
 
 class App extends React.Component {
   render(){
