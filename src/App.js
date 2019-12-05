0013ee3ead112748b780c47f678e0450081b0aa0
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-05 14:17:03 +0530

    Child component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import './App.css';
+import InputSearch from './components/InputSearch'
+import FilterResult from './components/FilterResult'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <InputSearch />
+        <FilterResult />
+      </div>
+    );
+  }
 }
 
 export default App;
