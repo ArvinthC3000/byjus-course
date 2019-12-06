3b6999a16dd4ced48e926df58c2f48b7e7b77063
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-06 12:46:08 +0530

    Pagination styling

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
-const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+const Pagination = ({ postsPerPage, totalPosts, paginate, loading }) => {
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
     pageNumbers.push(i);
   }
+  if (loading) {
+    return null;
+  }
 
   return (
     <nav>
+      <div>page no:</div>
       <ul className='pagination'>
         {pageNumbers.map(number => (
           <li key={number} className='page-item'>
