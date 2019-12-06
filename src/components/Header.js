64d947a20609c4ed0c8d9b87d46893dbac601601
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-06 15:53:32 +0530

    Header event handlers

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
 import React from 'react'
+// import Image from '../img/img1.png'
 
-const Header = (loading) => {
-    return (
-        <div>
-            Header component
+const Header = ({length, submitHandler, searchFilter}) => {
+    
+//   if (loading) {
+//     return (<img src={Image} alt="" width="50%" srcset=""/>)
+//   }
+
+return (
+    <div className="header_component">
+        <div className="header_title">
+            <h2>Find your dream course here</h2><span><p>Courses available: {length}</p></span>
         </div>
-    )
+        <form onSubmit={submitHandler}>
+        <input type="text" name="childSubject" onChange={searchFilter} placeholder="Search by provider or child subject..."/>
+        <button className="header_search">Search</button>
+        </form>
+    </div>
+
+)
 }
 
 export default Header
