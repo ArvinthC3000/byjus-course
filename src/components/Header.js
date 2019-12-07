5396ac54dbcd9b283a150c693ace945b605234a4
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-07 19:45:08 +0530

    Searc button and submit handler bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from '../img/img1.png'
 
-const Header = ({length, submitHandler, searchFilter, loading}) => {
+const Header = ({length, searchFilter, loading}) => {
     
   if (loading) {
     return (<img src={Image} alt="" width="50%" srcset=""/>)
@@ -12,9 +12,8 @@ return (
         <div className="header_title">
             <h2>Find your dream course here</h2><span><p>Courses available: {length}</p></span>
         </div>
-        <form onSubmit={submitHandler}>
+        <form>
         <input type="text" name="childSubject" onChange={searchFilter} placeholder="Search by provider or child subject..."/>
-        <button className="header_search">Search</button>
         </form>
     </div>
 
